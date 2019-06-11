07607ac17d28294ae3c6de725ee480f226772e61
Amber Taranto <ambzzzz@gmail.com>
2019-06-11 00:56:29 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,27 +1,28 @@
 def first_challenge
-  contacts = {
-    "Jon Snow" => {
-      name: "Jon",
-      email: "jon_snow@thewall.we",
-      favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
-      knows: nil
-    },
-    "Freddy Mercury" => {
-      name: "Freddy",
-      email: "freddy@mercury.com",
-      favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
+    contacts = {
+        "Jon Snow" => {
+            name: "Jon",
+            email: "jon_snow@thewall.we",
+            favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
+            knows: nil
+        },
+        "Freddy Mercury" => {
+            name: "Freddy",
+            email: "freddy@mercury.com",
+            favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
+        }
+
     }
 
-  }
+    contacts.each do |person, data|
 
-  contacts.each do |person, data|
+        data.each do |attribute, value|
 
-    data.each do |attribute, value|
-
-        if attribute == :favorite_icecream_flavors
-            value.remove("strawberry")
+            if attribute == :favorite_icecream_flavors
+                value.remove("strawberry")
+            end
         end
     end
-  #remember to return your newly altered contacts hash!
-  contacts
+      #remember to return your newly altered contacts hash!
+    contacts
 end
