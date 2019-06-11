49ab56855709811d810de23c42b2a7519ff2ebe1
Amber Taranto <ambzzzz@gmail.com>
2019-06-11 01:10:39 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,17 +1,17 @@
 def second_challenge
-  groceries = {
-   dairy: ["milk", "yogurt", "cheese"],
-   vegetable: ["carrots", "broccoli", "cucumbers"],
-   meat: ["chicken", "steak", "salmon"],
-   grains: ["rice", "pasta"]
-  }
+    groceries = {
+        dairy: ["milk", "yogurt", "cheese"],
+        vegetable: ["carrots", "broccoli", "cucumbers"],
+        meat: ["chicken", "steak", "salmon"],
+        grains: ["rice", "pasta"]
+    }
 
-  groceries.each do |grocery_type, data|
+    groceries.each do |grocery_type, data|
+        data.each do |food_type, value|
 
+            puts value.flatten
+      end
 
   end
-
-
-
 
 end
